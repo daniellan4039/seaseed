d232da2fb2ee5b12fa369448b96af1be060267fb
lanjian <lanjian4039@aliyun.com>
2021-08-26 07:00:15 +0800

    init: create a lib

--- README.md
+++
@@ -1,6 +0,0 @@
-### need filter _test for companyTree for user, but these exclude
-13222222222
-15080868457
-18285753038
-12300000000
-13133336666
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+module.exports = {
+    mode: "production",
+    watch: false,
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'build')
+    },
+    devtool: 'eval',
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']k
+                    }
+                }
+            }
+        ]
+    }
+}
