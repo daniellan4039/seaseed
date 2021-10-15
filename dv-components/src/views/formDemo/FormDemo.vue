2cccaced1ed2abe55df3d884a5ee25045347d6be
lanjian <lanjian4039@aliyun.com>
2021-10-15 10:51:58 +0800

    漏洞修复

--- dv-components/src/views/formDemo/FormDemo.vue
+++ dv-components/src/views/formDemo/FormDemo.vue
@@ -1,23 +1,43 @@
 <template>
   <div class="container">
     <a-divider></a-divider>
-    <a-form ref="formRef" :model="form.formModel" :rules="form.rules"
-            :label-col="{span:6}" :wrapper-col="{span: 12}">
-      <a-form-item label="User Name" name="userName">
-        <a-input v-model:value="form.formModel.userName" />
+    <a-form ref="formRef"
+            :model="form.formModel"
+            :rules="form.rules"
+            :label-col="{span:6}"
+            :wrapper-col="{span: 12}">
+      <a-form-item
+        label="User Name"
+        name="userName">
+        <a-input
+          v-model:value="form.formModel.userName" />
       </a-form-item>
-      <a-form-item label="Department" name="departmentId" v-if="form.formMap['departmentId'].visible">
+      <a-form-item
+        label="Department"
+        name="departmentId"
+        v-if="form.formMap['departmentId'].visible">
         <a-select v-model:value="form.formModel.departmentId"
                   :options="form.formMap['departmentId'].options"></a-select>
       </a-form-item>
-      <a-form-item label="Sex" name="sex" v-if="form.formMap['sex'].visible">
-        <a-select v-model:value="form.formModel.sex" :options="form.formMap['sex'].options"></a-select>
+      <a-form-item
+        label="Sex"
+        name="sex"
+        v-if="form.formMap['sex'].visible">
+        <a-select
+          v-model:value="form.formModel.sex"
+          :options="form.formMap['sex'].options"></a-select>
       </a-form-item>
-      <a-form-item label="Nickname" name="nickName">
-        <a-input v-model:value="form.formModel.nickName" />
+      <a-form-item
+        label="Nickname"
+        name="nickName">
+        <a-input
+          v-model:value="form.formModel.nickName" />
       </a-form-item>
-      <a-form-item label='Action'>
-        <a-button type='primary' @click='onSubmit'>Submit</a-button>
+      <a-form-item
+        label='Action'>
+        <a-button
+          type='primary'
+          @click='onSubmit'>Submit</a-button>
       </a-form-item>
     </a-form>
   </div>
