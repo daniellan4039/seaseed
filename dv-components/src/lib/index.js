24b916d35ec0014d44054d219fed1bc7adb835b9
lanjian <lanjian4039@aliyun.com>
2021-10-20 13:42:32 +0800

    df

--- dv-components/src/lib/index.js
+++
@@ -1,9 +0,0 @@
-import DvFormDef from '@/lib/form/DvFormDef'
-import DvInputDef from '@/lib/form/DvInputDef'
-import DvSelectDef from '@/lib/form/DvSelectDef'
-
-export default {
-    DvFormDef,
-    DvInputDef,
-    DvSelectDef
-}
--- dv-components/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import 'ant-design-vue/dist/antd.css';
-import Antd from 'ant-design-vue';
-import router from './router'
-
-
-createApp(App).use(Antd).use(router).mount('#app')
